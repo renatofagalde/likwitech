bb8f8fd6be7a777d3194c8e6a69bff8dd507879e
Renato Fagalde <renato@likwi.com.br>
2021-09-15 14:29:08 -0300

    no-sidebar-01 (#6)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -17,15 +17,7 @@ import {
     Tag
 } from 'antd';
 
-import {
-    DesktopOutlined,
-    FileOutlined,
-    LoadingOutlined,
-    PieChartOutlined,
-    PlusOutlined,
-    TeamOutlined,
-    UserOutlined
-} from '@ant-design/icons';
+import {LoadingOutlined, PlusOutlined, UserOutlined} from '@ant-design/icons';
 
 
 import './App.css';
@@ -192,6 +184,32 @@ function App() {
     }
 
     return <Layout style={{minHeight: '100vh'}}>
+        {/*
+        <Sider collapsible collapsed={collapsed}
+               onCollapse={setCollapsed}>
+            <div className="logo"/>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<PieChartOutlined/>}>
+                    Option 1
+                </Menu.Item>
+                <Menu.Item key="2" icon={<DesktopOutlined/>}>
+                    Option 2
+                </Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="9" icon={<FileOutlined/>}>
+                    Files
+                </Menu.Item>
+            </Menu>
+        </Sider>
+*/}
         <Layout className="site-layout">
             <Header className="site-layout-background" style={{padding: 0}}/>
             <Content style={{margin: '0 16px'}}>
